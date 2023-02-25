a11420dbb35e916fbb215180fbc257abdcb53367
mateus3007 <81122922+mateus3007@users.noreply.github.com>
2023-02-25 18:07:25 -0300

    Add files via upload

--- banco/src/main.java
+++ banco/src/main.java
@@ -1,10 +1,10 @@
 public class main {
   public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente mateus = new Cliente();
+		mateus.setNome("mateus");
 		
-		ContaCorrente cc = new ContaCorrente(venilton);
-		ContaPoupanca poupanca = new ContaPoupanca(venilton);
+		ContaCorrente cc = new ContaCorrente(mateus);
+		ContaPoupanca poupanca = new ContaPoupanca(mateus);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
